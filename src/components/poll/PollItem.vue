a2bf5842a6bddf2f4b694c37397c5694b257a799
Sergey Ladutko <verus774@mail.ru>
2018-01-03 23:42:22 +0300

    add poll list

---
+++ src/components/poll/PollItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <tr>
+    <td>{{ poll.title }}</td>
+    <td>
+      <div class="btn-group">
+        <button class="btn btn-success" title="Start">
+          <span class="glyphicon glyphicon-play" aria-hidden="true"></span>
+        </button>
+        <button class="btn btn-primary" title="Stop">
+          <span class="glyphicon glyphicon-stop" aria-hidden="true"></span>
+        </button>
+        <a class="btn btn-warning" title="Edit">
+          <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+        </a>
+        <button class="btn btn-danger" title="Remove">
+          <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+        </button>
+      </div>
+    </td>
+  </tr>
+
+</template>
+
+<script>
+  export default {
+    props: ['poll']
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/poll/PollList.vue
+++ src/components/poll/PollList.vue
@@ -4,12 +4,51 @@
       <div class="page-header text-center">
         <h3>Polls</h3>
       </div>
+      <table class="table table-hover">
+        <thead>
+        <tr>
+          <th>Title</th>
+          <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <poll-item
+          v-for="poll in polls"
+          :poll="poll"
+          :key="poll._id">
+        </poll-item>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
+  import PollItem from './PollItem'
+  import {HTTP} from '../../http/http-common'
 
+  export default {
+    components: {PollItem},
+    data () {
+      return {
+        polls: []
+      }
+    },
+    methods: {
+      loadData () {
+        HTTP.get('polls')
+          .then(res => {
+            this.polls = res.data.data
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
+    },
+    created () {
+      this.loadData()
+    }
+  }
 </script>
 
 <style scoped>
